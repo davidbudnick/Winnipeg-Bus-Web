5ad9b7602c949324aa8ea12ad2c045b93cb0db20
David Budnick <david@budnick.ca>
2019-01-11 14:21:54 -0600

    stop data received in json

--- api/src/routes/stop.js
+++ api/src/routes/stop.js
@@ -4,7 +4,9 @@ const stop = require('../stop');
 const pino = require('pino');
 const logger = pino({ prettyPrint: { colorize: true }, level: process.env.LOG_LEVEL || 'info', name: 'index' });
 
-router.get('/:stopNumber/busNumber', async (req, res, next) => {
+router.get('/:stopNumber/:busNumber', async (req, res, next) => {
   let stopData = await stop.getBusData(req.params.stopNumber, req.params.busNumber);
   res.send(stopData);
 });
+
+module.exports = router;
--- api/src/stop.js
+++ api/src/stop.js
@@ -4,29 +4,52 @@ const logger = pino({ prettyPrint: { colorize: true }, level: process.env.LOG_LE
 const db = require('../db');
 const axios = require('axios');
 
+/**
+ *
+ * @param {Stop Number} stopNumber
+ * @param {Bus Number} busNumber
+ * @returns {Query of all the bus data from the specifc bus number}
+ */
 async function getBusData(stopNumber, busNumber) {
+  //Check for information about the transit system
+  let systemStatus = await checkSystem();
+
+  //checks if the transit system is "regular" or online
+  if (systemStatus === 'regular') {
+    let url = `https://api.winnipegtransit.com/v3/stops/${stopNumber}/schedule.json?max-results-per-route=50&api-key=${
+      process.env.TRANSIT_API_KEY
+    }`;
+
+    //Query all the bus data
+    let busData = await axios({
+      method: 'get',
+      url,
+    }).catch((err) => {
+      logger.error(`There was an error looking up this ${stopNumber} stop number`, err);
+    });
+    // prettier-ignore
+    return busData.data["stop-schedule"]["route-schedules"]
+  } else {
+    return `Bus System down with a status of ${systemStatus}`;
+  }
+}
+
+/**
+ * Checks if the bus trasit sysem is active
+ * @returns If the system is active it will return 'regular' if it returns anything else the system is down
+ */
+async function checkSystem() {
   //Check if the bus system is actice
-  let checkUrl = `https://api.winnipegtransit.com/v3/statuses/schedule.json?api-key=${process.env.TRANSIT_API_KEY}`;
+  let url = `https://api.winnipegtransit.com/v3/statuses/schedule.json?api-key=${process.env.TRANSIT_API_KEY}`;
 
   let systemData = await axios({
     method: 'get',
     url,
   }).catch((err) => {
-    logger.error('There was an error looking up the status of the bus system');
+    logger.error('There was an error looking up the status of the bus system', err);
   });
 
-  logger.info(systemData);
-
-  let url = `https://api.winnipegtransit.com/v3/stops/${stopNumber}.json?api-key=${process.env.TRANSIT_API_KEY}`;
-  //Query all the bus data
-  let busData = await axios({
-    method: 'get',
-    url,
-  }).catch((err) => {
-    logger.error(`There was an error looking up this ${stopNumber} stop number`, err);
-  });
-
-  return busData;
+  return systemData.data.status.value;
 }
 
 module.exports.getBusData = getBusData;
